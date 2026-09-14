d4a4d6fe158c428d001276159e1215e4160b7786
Pokelote <clem.teboul@gmail.com>
2026-09-14 07:43:22 +0200

    Force SW update check on load

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "mon-espace-v19";
+const CACHE_NAME = "mon-espace-v20";
 const APP_SHELL = ["./mon-espace.html", "./manifest.json", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
