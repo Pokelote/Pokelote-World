86e5081c337226f3bf2760ff6c48cb23f48c65ea
Pokelote <clem.teboul@gmail.com>
2026-09-14 15:32:35 +0200

    Bump cache v36

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "mon-espace-v35";
+const CACHE_NAME = "mon-espace-v36";
 const APP_SHELL = ["./mon-espace.html", "./manifest.json", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
