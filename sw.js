d97ae7573072ba62f27bec3d027319d43ef96c31
Pokelote <clem.teboul@gmail.com>
2026-09-17 10:43:13 +0200

    Mise a jour quotidienne

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "mon-espace-v49";
+const CACHE_NAME = "mon-espace-v50";
 const APP_SHELL = ["./mon-espace.html", "./manifest.json", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
