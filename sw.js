12144005ce99ff343aadc9b272d74c222de4d638
Pokelote <clem.teboul@gmail.com>
2026-09-22 07:13:20 +0200

    Bump cache version to v54

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "mon-espace-v53";
+const CACHE_NAME = "mon-espace-v54";
 const APP_SHELL = ["./mon-espace.html", "./manifest.json", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
